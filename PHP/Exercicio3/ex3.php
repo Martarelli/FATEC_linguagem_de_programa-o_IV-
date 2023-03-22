95c5732c9b24ab9a8e52f242eaf41958aeb8d3b6
Martarelli <renanmartarelli@gmail.com>
2023-03-21 23:32:20 -0300

    exercicio com for

---
+++ PHP/Exercicio3/ex3.php
@@ -0,0 +1,13 @@
+<?php 
+$numero = 100;
+
+for($i = 0 ; $i <= $numero; $i++){
+    if($i % 2 === 0){
+        echo "<b>".$i."</b>"
+    } else {
+        echo "<span>".$i."</span>"
+    }
+}
+
+
+?>
