273a438d16c513ca169fa834122ee47411e5d2c6
Martarelli <renanmartarelli@gmail.com>
2023-03-21 23:35:52 -0300

    exercicio 2

--- PHP/Exercicio2/ex2.php
+++ PHP/Exercicio2/ex2.php
@@ -1,6 +1,7 @@
 <?php
 $cars = array("Volvo", "BMW", "Toyota");
-
+var_dump($cars);
+print_r($cars);
 foreach($cars as $c){
     echo "</br>$c";
 }
--- PHP/Exercicio3/ex3.php
+++ PHP/Exercicio3/ex3.php
@@ -3,9 +3,10 @@ $numero = 100;
 
 for($i = 0 ; $i <= $numero; $i++){
     if($i % 2 === 0){
-        echo "<b>".$i."</b>"
+        echo "<b>".$i."</b></br>";
+        
     } else {
-        echo "<span>".$i."</span>"
+        echo "<span>".$i."</span></br>";
     }
 }
 
