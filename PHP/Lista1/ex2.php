f6946e0bd0936c906e0e3a855f5badba3d147f8a
Martarelli <renanmartarelli@gmail.com>
2023-03-26 23:58:58 -0300

    exemplo 2 ok

--- PHP/Lista1/ex2.php
+++ PHP/Lista1/ex2.php
@@ -1,1 +1,71 @@
-<h1>WORKING</h1>
+<?php
+$flag_msg = null;
+$msg = "";
+
+if (isset($_GET['enviar'])) {
+  
+  $preco = $_GET["preco"];
+  $qtd = $_GET["qtd"];
+
+  if (!empty($preco) && !empty($qtd) && 
+      is_numeric($preco) && is_numeric($qtd) &&
+      $preco >= 0 && $qtd >= 0) {
+  
+    $total = $preco * $qtd;
+  
+    $flag_msg = true; // Sucesso 
+    $msg = "Cálculo efetuado com sucesso:<br />Valor Total = R$"; 
+    $msg .= number_format($total, 2);
+
+    
+  }else{  
+    $flag_msg = false; //Erro 
+    $msg = "Dados incorretos, preencha o formulário corretamente!";
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+    <title>VALOR TOTAL</title>
+</head>
+<body>
+<div class="p-4 mb-4 bg-light">
+  <h1 class="display-5">VALOR TOTAL</h1>
+  <hr class="my-3">
+  <p class="lead">Esse exemplo calcula o valor total.</p>
+</div>
+
+<div class="container">
+  <form method="GET">
+    <div class="form-group col-md-2">
+      <label for="preco">Preco:</label>
+      <input type="text" class="form-control" id="preco" name="preco" required>
+    </div>
+    <br />
+    <div class="form-group col-md-2">
+      <label for="qtd">Quantidade:</label>
+      <input type="text" class="form-control" id="qtd" name="qtd" required>
+    </div>
+    <br />
+    <button type="submit" class="btn btn-primary mb-2" name="enviar">Enviar</button>
+    <a href="ex1.php"><button type="button" class="btn btn-primary mb-2" name="limpar">Limpar</button></a>
+  </form>
+  <?php 
+    if (!is_null($flag_msg)) {
+      if ($flag_msg) {
+        echo "<div class='alert alert-success' role='alert'>$msg</div>"; 
+      }else{
+        echo "<div class='alert alert-warning' role='alert'>$msg</div>"; 
+      }
+    }
+?>
+</div>
+</body>
+</html>
