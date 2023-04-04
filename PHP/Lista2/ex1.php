3296cb102f9b9417470ddca932b0e652d2f948fd
Martarelli <renanmartarelli@gmail.com>
2023-04-04 04:16:54 -0300

    exercicio 1 finalizado

--- PHP/Lista2/ex1.php
+++ PHP/Lista2/ex1.php
@@ -20,7 +20,7 @@ while ($a <= 200) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
-    <title>TROCO</title>
+    <title>QUADRADO</title>
 </head>
 <body>
 <div class="p-4 mb-4 bg-light">
