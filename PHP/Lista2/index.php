ff34585c1182f855a31a60d321c578340765fa98
Martarelli <renanmartarelli@gmail.com>
2023-04-04 23:24:32 -0300

    index exercicios lista 2

---
+++ PHP/Lista2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+    <title>LISTA 2</title>
+</head>
+<body class="d-flex flex-column align-items-center justify-content-center">
+<div class="p-4 mb-4 bg-light w-100 text-center ">
+  <h1 class="display-5">LISTA 2</h1>
+  <hr class="my-3">
+  <p class="lead">LINKS EXERCICIO DA LISTA 2.</p>
+</div>
+<div class="list-group m-5 w-25 text-center">
+  <a href="ex1.php" class="list-group-item list-group-item-action">Exercicio 1</a>
+  <a href="ex2.php" class="list-group-item list-group-item-action">Exercicio 2</a>
+  <a href="ex3.php" class="list-group-item list-group-item-action">Exercicio 3</a>
+  <a href="ex4.php" class="list-group-item list-group-item-action">Exercicio 4</a>
+  <a href="ex5.php" class="list-group-item list-group-item-action">Exercicio 5</a>
+  <a href="ex6.php" class="list-group-item list-group-item-action">Exercicio 6</a>
+  <a href="ex7.php" class="list-group-item list-group-item-action">Exercicio 7</a>
+  <a href="ex8.php" class="list-group-item list-group-item-action">Exercicio 8</a>
+</div>
+</body>
+</html>
