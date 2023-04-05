8dd91e8eecfd521bf88b79ba4894e10232c03b32
Martarelli <renanmartarelli@gmail.com>
2023-04-04 23:50:45 -0300

    exercicio 7

--- PHP/Lista2/ex7.php
+++ PHP/Lista2/ex7.php
@@ -1,48 +1,27 @@
 <?php
-$flag_msg = true;
+$flag_msg = null;
 $msg = "";
-$numeros = array();
 
-for ($i=0; $i < 50 ; $i++) { 
-  $n = random_int(0,100);
-  array_push($numeros, $n);
-}
+if (isset($_GET['enviar'])) {
 
-$menor = 0;
-$menorIndex = 0;
-$maior = 0;
-$maiorIndex = 0;
+  $texto = $_GET["texto"];
+  $palavra = $_GET["palavra"];
 
-$soma = 0;
-foreach ($numeros as $key => $value) {
-  if($key === 0){
-    $menor = $value;
-    $menorIndex = $key;
-    $maior = $value;
-    $maiorIndex = $key;
+  if (!empty($palavra) && !empty($texto)) {
+    $flag_msg = true; // Sucesso 
+    $existe = strpos($texto, $palavra);
+
+    if (!$existe) {
+      $msg .= "A palavra não existe no texto";
+    } else {
+      $msg .= "A palavra existe no texto";
+    }
+
+  } else {
+    $flag_msg = false;
+    $msg .= "Preencha os campos corretamente";
   }
-
-  if($value > $maior){
-    $maiorIndex = $key;
-    $maior = $value;
-  }
-
-  if($value < $menor){
-    $menorIndex = $key;
-    $menor = $value;
-  }
-
-  $soma += $value;
-}
-$media = $soma / count($numeros);
-
-$msg .= "SOMA = " . number_format($soma,0) . "<br/>";
-$msg .= "MEDIA = " . number_format($media,2) . "<br/>";
-$msg .= "MAIOR = " . number_format($maior) . " INDICE DO MAIOR = " . $maiorIndex . "<br/>";
-$msg .= "MENOR = " . number_format($menor) . " INDICE DO MENOR = " . $menorIndex . "<br/>";
-
-
-
+} 
 
 ?>
 
@@ -54,25 +33,30 @@ $msg .= "MENOR = " . number_format($menor) . " INDICE DO MENOR = " . $menorIndex
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
-    <title>ARRAY</title>
+    <title>TEXTO</title>
 </head>
 <body>
 <div class="p-4 mb-4 bg-light">
-  <h1 class="display-5">Exibe a array de números inteiros</h1>
+  <h1 class="display-5">Exibe a palavra e um texto</h1>
   <hr class="my-3">
-  <p class="lead">Esse exercicio apresenta exibir a soma dos elementos do array, a média dos elementos, o menor e o maior elemento e a suas respectivas posições do array.</p>
+  <p class="lead">Esse exercicio realiza uma busca no texto e exiba a posição inicial e final da palavra no texto.</p>
 </div>
 
 <div class="container">
 <form method="GET">
-  <div class="form-group col-md-2">
-    <label for="numero">Informe um número:</label>
-    <input type="text" class="form-control" id="num" name="num" required>
-  </div>
-  <br />
-  <button type="submit" class="btn btn-primary mb-2" name="enviar">Enviar</button>
-  <a href="ex6.php"><button type="button" class="btn btn-primary mb-2" name="limpar">Limpar</button></a>
-</form>
+      <div class="form-group col-md-2">
+        <label for="texto">Texto:</label>
+        <input type="text" class="form-control" id="texto" name="texto" required>
+      </div>
+      <br />
+      <div class="form-group col-md-2">
+        <label for="palavra">Palavra:</label>
+        <input type="text" class="form-control" id="palavra" name="palavra" required>
+      </div>
+      <br />
+      <button type="submit" class="btn btn-primary mb-2" name="enviar">Enviar</button>
+      <a href="ex7.php"><button type="button" class="btn btn-primary mb-2" name="limpar">Limpar</button></a>
+    </form>
     <?php 
     if (!is_null($flag_msg)) {
       if ($flag_msg) {
