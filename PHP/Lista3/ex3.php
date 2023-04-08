d48b1dfba11a206466463985b5dce50536559ac0
Martarelli <renanmartarelli@gmail.com>
2023-04-08 18:14:25 -0300

    exercicio 3 iniciado

---
+++ PHP/Lista3/ex3.php
@@ -0,0 +1,3 @@
+<!-- Crie um documento HTML que permita incluir compromissos. Os campos solicitados
+devem ser: usuário, compromisso, local, data e hora. Os compromissos devem ser
+gravados em um arquivo no formato JSON. Crie também uma página que liste os compromissos de um determinado usuário e permita excluir compromissos. -->
