401db93213d0f32e4759e7bb872000f19247bbae
Martarelli <renanmartarelli@gmail.com>
2023-04-11 20:01:28 -0300

    exercicio 2 concluido

--- PHP/1º Bimestre/Prova P1/ex2.php
+++ PHP/1º Bimestre/Prova P1/ex2.php
@@ -8,8 +8,36 @@ for ($i=0; $i < 3; $i++) {
         $n = random_int(0, 100);
         array_push($vet, $n);
     }
-    array_push($vetor, $vet)
+    sort($vet);
+    array_push($vetor, $vet);
 }
 
+$repetidos = array();
+for ($i=0; $i < 1; $i++) {
+    foreach ($vetor[$i] as $num) {
+        $contem = 0;
+        foreach($vetor[1] as $num1){
+            if($num === $num1){
+                $contem++;
+                break;
+            }
+        }
+        foreach($vetor[2] as $num2){
+            if($num === $num2){
+                $contem++;
+                break;
+            }
+        }
+        if($contem === 2){
+            array_push($repetidos, $num);
+        }
+    }
+}
+foreach ($vetor as $key => $value) {
+   print_r($value);
+   echo "<br/>";
+}
+echo "VETOR NÚMEROS REPETIDOS: ";
+print_r($repetidos);
 
 ?>
