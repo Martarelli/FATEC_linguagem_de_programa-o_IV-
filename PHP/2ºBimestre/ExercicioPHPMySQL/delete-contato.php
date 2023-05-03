42477d4083f2dbe7e60595e6bc94cc54537111c4
Martarelli <renanmartarelli@gmail.com>
2023-05-02 21:39:20 -0300

    correção

--- PHP/2ºBimestre/ExercicioPHPMySQL/delete-contato.php
+++ PHP/2ºBimestre/ExercicioPHPMySQL/delete-contato.php
@@ -6,8 +6,6 @@ if (isset($_GET['id'])) {
 require_once('connection.php');
 //CREATE QUERY SQL
 $mysql_query = "DELETE FROM contatos WHERE id={$id}";
-//CLOSE CONNECTION TO DATABASE
-mysqli_close($conn);
 
 if ($conn->query($mysql_query) === TRUE) {
     $msg = "delete success";
@@ -18,6 +16,7 @@ else {
     $msgerror = $conn->error;
 }
 
+//CLOSE CONNECTION TO DATABASE
 mysqli_close($conn);
 
 header("Location: contatos.php?msg={$msg}&msgerror={$msgerror}");
