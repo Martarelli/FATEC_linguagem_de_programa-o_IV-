1975d654a25548f356eab3c130b2669719b22de4
Martarelli <renanmartarelli@gmail.com>
2023-05-02 21:34:14 -0300

    delete method

--- PHP/2ºBimestre/ExercicioPHPMySQL/delete-contato.php
+++ PHP/2ºBimestre/ExercicioPHPMySQL/delete-contato.php
@@ -1,4 +1,26 @@
 <?php
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
 
+//CONNECTION TO DATABASE
+require_once('connection.php');
+//CREATE QUERY SQL
+$mysql_query = "DELETE FROM contatos WHERE id={id}";
+//CLOSE CONNECTION TO DATABASE
+mysqli_close($conn);
+}
+
+if ($conn->query($mysql_query) === TRUE) {
+    $msg = "delete success";
+    $msgerror = "";
+} 
+else {
+    $msg = "delete error";
+    $msgerror = $conn->error;
+}
+
+mysqli_close($conn);
+
+header("Location: contatos.php?msg={$msg}&msgerror={$msgerror}");
 
 ?>
