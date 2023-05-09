9139f014a5e0ac3bd0a19bab916fbdde1ed82d02
Martarelli <renanmartarelli@gmail.com>
2023-05-09 19:50:57 -0300

    update

--- PHP/2ºBimestre/ExercicioPHPMySQL/update-contato.php
+++ PHP/2ºBimestre/ExercicioPHPMySQL/update-contato.php
@@ -1,16 +1,23 @@
 <?php
 
 require("header-inc.php");	
+require_once('connection.php');
+
+if (isset($_GET['id'])){
+	$id = $_GET['id'];
+	$mysql_query = "SELECT * FROM contatos WHERE id={$id}";
+	$result = $conn->query($mysql_query);
+	$row = mysqli_fetch_assoc($result);
+}
 
 if (isset($_POST['enviar']))
 {
-	require_once('connection.php');
 	$id = $_POST['id'];
 	$nome = $_POST['nome'];
 	$email = $_POST['email'];
 	$datanasc = $_POST['datanasc'];
 
-	$mysql_query = "UPDATE contatos SET nome = '{$nome}', email = {$email}, datanasc = {$datanasc} WHERE id = {$id}";
+	$mysql_query = "UPDATE contatos SET nome = '{$nome}', email = '{$email}', datanasc = '{$datanasc}' WHERE id = '{$id}'";
 
 	if ($conn->query($mysql_query) === TRUE)
 	{
@@ -21,10 +28,10 @@ if (isset($_POST['enviar']))
 		$msgerror = $conn->$error;
 	}
 
-	mysqli_close($conn);
-
+	
 	header('Location: contatos.php?msg={$msg}&msgerror={$msgerror}');
 }
+mysqli_close($conn);
 ?>
 <div class="container">
 	<h2>Contatos</h2>
@@ -32,13 +39,13 @@ if (isset($_POST['enviar']))
   	<hr>  	
 	<div class="wrapper">
 		<form method="post">
-			<input type="hidden" name="id" value="">
+			<input type="hidden" name="id" value="<?= $row['id']; ?>">
 			<label for="name">&nbsp;Nome</label>
-			<input type="text" name="nome" id="nome" class="form-control" required value=""><br>
+			<input type="text" name="nome" id="nome" class="form-control" required value="<?= $row['nome']; ?>"><br>
 			<label for="email">&nbsp;E-Mail</label>
-			<input type="text" name="email" id="email" class="form-control"required value=""><br>
+			<input type="text" name="email" id="email" class="form-control"required value="<?= $row['email']; ?>"><br>
 			<label for="datanasc">&nbsp;Data de Nascimento</label>
-			<input type="date" name="datanasc" id="datanasc" class="form-control" style="width: 200px;" value=""><br>
+			<input type="date" name="datanasc" id="datanasc" class="form-control" style="width: 200px;" value="<?= $row['datanasc']; ?>"><br>
 			<input type="submit" name="enviar" value="Atualizar" class="btn btn-primary w100">
 		</form>
 	</div>
